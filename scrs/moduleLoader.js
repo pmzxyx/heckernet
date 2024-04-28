134c6bc421ee07baf988757d2cf23faafc63aad8
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2024-04-28 15:44:19 -0700

    Update moduleLoader.js

--- scrs/moduleLoader.js
+++ scrs/moduleLoader.js
@@ -1,6 +1,12 @@
 (function(window) {
   let fonts = [
-      
+      'https://cdn.jsdelivr.net/gh/pmzxyx/heckernet@main/scrs/scratch/fonts/Grand9K-Pixel.ttf',
+      'https://cdn.jsdelivr.net/gh/pmzxyx/heckernet@main/scrs/scratch/fonts/Griffy-Regular.ttf',
+      'https://cdn.jsdelivr.net/gh/pmzxyx/heckernet@main/scrs/scratch/fonts/Knewave.ttf',
+      'https://cdn.jsdelivr.net/gh/pmzxyx/heckernet@main/scrs/scratch/fonts/NotoSans-Medium.ttf',
+      'https://cdn.jsdelivr.net/gh/pmzxyx/heckernet@main/scrs/scratch/fonts/Scratch.ttf',
+      'https://cdn.jsdelivr.net/gh/pmzxyx/heckernet@main/scrs/scratch/fonts/SourceSerifPro-Regular.otf',
+      'https://cdn.jsdelivr.net/gh/pmzxyx/heckernet@main/scrs/scratch/fonts/handlee-regular.ttf',
     ];
   let modules = [
     
